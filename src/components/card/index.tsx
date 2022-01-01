b4fb7a326e0615bf19232391883de509ca9a9e6b
ishii-tomasu <tomasu.ishii@albedojapan.com>
2022-01-01 17:18:17 +0900

    カードアイコン調整

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -9,10 +9,10 @@ type propsCard = {
 export default function Card(props: propsCard) {
   return (
     <>
-      <a href={props.link} className="flex justify-center items-center w-[92px] h-[92px] bg-white rounded-lg">
-        <span className="text-5xl">{props.emoji}</span>
+      <a href={props.link} className="flex justify-center items-center sm:w-[92px] sm:h-[92px] w-[76px] h-[76px] bg-white rounded-lg">
+        <span className="sm:text-5xl text-4xl">{props.emoji}</span>
       </a>
-      <div className="w-[calc(100%-108px)]">
+      <div className="sm:w-[calc(100%-108px)] w-[calc(100%-92px)]">
         <h3 className="font-bold text-gray-600">{props.title}</h3>
         <div className="mt-2">{props.user}</div>
       </div>
