650f6afb3e20dfa59ce622175024e88b6e55439c
ishii-tomasu <tomasu.ishii@albedojapan.com>
2022-01-01 17:06:52 +0900

    cardの上下余白調整

--- src/components/card/cards.tsx
+++ src/components/card/cards.tsx
@@ -12,7 +12,7 @@ export default function Cards(props: {cards: propsCard[]}) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-x-5">
       {props.cards.map((card) => (
-        <article className="flex justify-between py-5">
+        <article className="flex justify-between py-4">
           <Card title={card.title} link={card.link} emoji={card.emoji} user={card.user} />
         </article>
       ))}
